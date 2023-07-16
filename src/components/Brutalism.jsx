3d4e76472f9784ec48057697dfeb70ab36946a91
Yulevski <yonaiyuu@gmail.com>
2023-07-16 20:28:38 +0900

    hover and names

--- src/components/Brutalism.jsx
+++ src/components/Brutalism.jsx
@@ -92,69 +92,156 @@ function UIpage() {
     <div className="brutalist-photos">
 
   <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image1)} >
+      <div class="text" >Aeronautical Museum Belgrade</div>
+    </div>
     <img src={image1} alt="belgrade1" className="photo" onClick={() => handleClick(image1)} />  
   </div>
+
   <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image2)} >
+      <div class="text" >Western Gate of Belgrade, Novi Beograd</div>
+    </div>
     <img src={image2} alt="belgrade2" className="photo" onClick={() => handleClick(image2)}/>
   </div>
+
   <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image3)} >
+      <div class="text" >Western Gate of Belgrade, Novi Beograd</div>
+    </div>
     <img src={image3} alt="belgrade3" className="photo" onClick={() => handleClick(image3)}/>
   </div>
+
   <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image4)} >
+      <div class="text" >A clock on the Dunav Penzije, Belgrade, Serbia </div>
+    </div>
   <img src={image4} alt="belgrade4" className="photo" onClick={() => handleClick(image4)} />
-</div>
+  </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image5)} >
+      <div class="text" >A building somehwhere in Belgrade</div>
+    </div>
   <img src={image5} alt="belgrade5" className="photo" onClick={() => handleClick(image5)} />
 </div>
+
 <div className="photo-container">
+  <div class="overlay" onClick={() => handleClick(image6)} >
+      <div class="text" >A building somehwhere in Belgrade</div>
+    </div>
   <img src={image6} alt="belgrade6" className="photo" onClick={() => handleClick(image6)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image7)} >
+      <div class="text" >Palace Serbia, Novigrad</div>
+    </div>
   <img src={image7} alt="belgrade7" className="photo" onClick={() => handleClick(image7)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image8)} >
+      <div class="text" >A control tower in the Belgrade airport</div>
+    </div>
   <img src={image8} alt="belgrade8" className="photo" onClick={() => handleClick(image8)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image9)} >
+      <div class="text" >A building somehwhere in Belgrade</div>
+    </div>
   <img src={image9} alt="belgrade9" className="photo" onClick={() => handleClick(image9)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image10)} >
+      <div class="text" >A building somehwhere in Novi Belgrade</div>
+    </div>
   <img src={image10} alt="belgrade10" className="photo" onClick={() => handleClick(image10)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image11)} >
+      <div class="text" >A Hotel near the central bus station</div>
+    </div>
   <img src={image11} alt="belgrade11" className="photo" onClick={() => handleClick(image11)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image12)} >
+      <div class="text" >Музеј савремене уметности, new belgrade,</div>
+    </div>
   <img src={image12} alt="belgrade12" className="photo" onClick={() => handleClick(image12)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image13)} >
+      <div class="text" >A building Novi Beograd</div>
+    </div>
   <img src={image13} alt="belgrade13" className="photo" onClick={() => handleClick(image13)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image14)} >
+      <div class="text" >A building Novi Beograd</div>
+    </div>
   <img src={image14} alt="belgrade14" className="photo" onClick={() => handleClick(image14)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image15)} >
+      <div class="text" >A block in Novi Beograd</div>
+    </div>
   <img src={image15} alt="belgrade15" className="photo" onClick={() => handleClick(image15)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image16)} >
+      <div class="text" >A block in Novi Beograd</div>
+    </div>
   <img src={image16} alt="belgrade16" className="photo" onClick={() => handleClick(image16)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image17)} >
+      <div class="text"> A restaurant in Novi Beograd</div>
+    </div>
   <img src={image17} alt="belgrade17" className="photo" onClick={() => handleClick(image17)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image18)} >
+      <div class="text" >A control tower in Belgrade airport</div>
+    </div>
   <img src={image18} alt="belgrade18" className="photo" onClick={() => handleClick(image18)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image19)} >
+      <div class="text" >A control tower in Belgrade airport</div>
+    </div>
   <img src={image19} alt="belgrade19" className="photo" onClick={() => handleClick(image19)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image20)} >
+      <div class="text" >Blocks, Novi Beograd, Serbia</div>
+    </div>
   <img src={image20} alt="belgrade20" className="photo" onClick={() => handleClick(image20)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image21)} >
+      <div class="text" >A building in Makedonska Street, Belgrade,Serbia</div>
+    </div>
   <img src={image21} alt="belgrade21" className="photo" onClick={() => handleClick(image21)} />
 </div>
+
 <div className="photo-container">
+    <div class="overlay" onClick={() => handleClick(image22)} >
+      <div class="text" >An Object in front of the restaurant Dining Sailor, Belgrade</div>
+    </div>
   <img src={image22} alt="belgrade22" className="photo" onClick={() => handleClick(image22)} />
 </div>
 
