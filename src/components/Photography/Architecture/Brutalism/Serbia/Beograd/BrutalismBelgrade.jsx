7e6cb664ecc17b50648603cbe1d57a701d3e8aca
Yulevski <yonaiyuu@gmail.com>
2023-07-26 14:49:34 +0900

    refactoring and webp are added

--- src/components/Photography/Architecture/Brutalism/Serbia/Beograd/BrutalismBelgrade.jsx
+++ src/components/Photography/Architecture/Brutalism/Serbia/Beograd/BrutalismBelgrade.jsx
@@ -24,6 +24,29 @@ import image19 from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/19
 import image20 from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/20.JPG";
 import image21 from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/21.jpg";
 import image22 from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/22.jpg";
+import image1w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/1.webp";
+import image2w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/2.webp";
+import image3w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/3.webp";
+import image4w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/4.webp";
+import image5w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/5.webp";
+import image51w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/51w.webp";
+import image6w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/6.webp";
+import image7w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/7.webp";
+import image8w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/8.webp";
+import image9w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/9.webp";
+import image10w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/10.webp";
+import image11w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/11.webp";
+import image12w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/12.webp";
+import image13w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/13.webp";
+import image14w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/14.webp";
+import image15w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/15.webp";
+import image16w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/16.webp";
+import image17w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/17.webp";
+import image18w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/18.webp";
+import image19w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/19.webp";
+import image20w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/20.webp";
+import image21w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/21.webp";
+import image22w from "./styles/pictures/Brutalist/architecture/Serbia/Belgrade/webp/22.webp"
 
 function UIpage() {
  
@@ -43,6 +66,7 @@ function UIpage() {
         width: window.innerWidth,
         height: window.innerHeight,
       });
+      
     };
 
     window.addEventListener('resize', handleResize);
@@ -51,20 +75,49 @@ function UIpage() {
     };
   }, []);
 
+  const handleClickOutsideImage = (event) => {
+    // Check if the click occurred outside the enlarged image
+    if (event.target.className === 'dialogue-overlay') {
+      setEnlargedImg(null);
+      console.log('Enlarged image closed');
+    }
+  };
+  useEffect(() => {
+    if (enlargedImg) {
+      console.log('Enlarged image opened:', enlargedImg);
+
+      // Attach the event listener to the whole document when the image is enlarged
+      document.addEventListener('click', handleClickOutsideImage);
+
+      // Return a cleanup function to remove the event listener when the component unmounts
+      return () => {
+        document.removeEventListener('click', handleClickOutsideImage);
+      };
+    }
+  }, [enlargedImg]);
+
   const calculateEnlargedImgSize = () => {
     if (enlargedImg) {
       const img = new Image();
       img.src = enlargedImg;
       const imgAspectRatio = img.width / img.height;
-      const maxWidth = windowSize.width * 0.8;
-      const maxHeight = windowSize.height * 0.8;
+      const maxWidth = windowSize.width * 0.7;
+      const maxHeight = windowSize.height * 0.9;
       
       let width = maxWidth;
       let height = maxWidth / imgAspectRatio;
 
-      if (height > maxHeight) {
-        height = maxHeight;
-        width = maxHeight * imgAspectRatio;
+      // if (height > maxHeight) {
+      //   height = maxHeight;
+      //   width = maxHeight * imgAspectRatio;
+      // }
+      if(img.height>img.width){
+          height=maxHeight;
+          width=maxHeight*imgAspectRatio;
+      }
+      if(img.height<img.width){
+          width=maxWidth;
+          height=maxWidth/imgAspectRatio;
       }
 
       return { width, height };
@@ -75,6 +128,21 @@ function UIpage() {
 
   const { width, height } = calculateEnlargedImgSize();
 
+
+
+  const PhotoContainer = ({ imageUrl, webpUrl, altText, overlayText, onClickHandler }) => {
+    return (
+      <div className="photo-container">
+        <div className="overlay" onClick={() => onClickHandler(imageUrl)}>
+          <div className="text">{overlayText}</div>
+        </div>
+        <picture>
+          {webpUrl && <source srcSet={webpUrl} type="image/webp" />}
+          <img src={imageUrl} alt={altText} className="photo" onClick={() => onClickHandler(imageUrl)} />
+        </picture>
+      </div>
+    );
+  };
   return( 
     <>
     <header className="site-header">
@@ -90,178 +158,216 @@ function UIpage() {
       </div>
     </header>
 
-    <div className="brutalist-photos">
+  <div className="brutalist-photos">
 
-  <div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image1)} >
-      <div class="text" >Aeronautical Museum Belgrade</div>
-    </div>
-    <img src={image1} alt="belgrade1" className="photo" onClick={() => handleClick(image1)} />  
-  </div>
+             {/* PhotoContainer for image 1 */}
+      <PhotoContainer
+          imageUrl={image1}
+          webpUrl={image1w}
+          altText="belgrade1"
+          overlayText="Western Gate of Belgrade, Novi Beograd"
+          onClickHandler={handleClick}
+        />
+                {/* PhotoContainer for image 2 */}
+        <PhotoContainer
+          imageUrl={image2}
+          webpUrl={image2w}
+          altText="belgrade2"
+          overlayText="Western Gate of Belgrade, Novi Beograd"
+          onClickHandler={handleClick}
+        />
+             {/* PhotoContainer for image 3 */}
+             <PhotoContainer
+          imageUrl={image3}
+          webpUrl={image3w}
+          altText="belgrade3"
+          overlayText="Western Gate of Belgrade, Novi Beograd"
+          onClickHandler={handleClick}
+        />
 
-  <div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image2)} >
-      <div class="text" >Western Gate of Belgrade, Novi Beograd</div>
-    </div>
-    <img src={image2} alt="belgrade2" className="photo" onClick={() => handleClick(image2)}/>
-  </div>
+        {/* PhotoContainer for image 4 */}
+        <PhotoContainer
+          imageUrl={image4}
+          webpUrl={image4w}
+          altText="belgrade4"
+          overlayText="A clock on the Dunav Penzije, Belgrade, Serbia"
+          onClickHandler={handleClick}
+        />
 
-  <div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image3)} >
-      <div class="text" >Western Gate of Belgrade, Novi Beograd</div>
-    </div>
-    <img src={image3} alt="belgrade3" className="photo" onClick={() => handleClick(image3)}/>
-  </div>
+        {/* PhotoContainer for image 5 */}
+        <PhotoContainer
+          imageUrl={image5}
+          webpUrl={image5w}
+          altText="belgrade5"
+          overlayText="A building somewhere in Belgrade"
+          onClickHandler={handleClick}
+        />
 
-  <div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image4)} >
-      <div class="text" >A clock on the Dunav Penzije, Belgrade, Serbia </div>
-    </div>
-  <img src={image4} alt="belgrade4" className="photo" onClick={() => handleClick(image4)} />
-  </div>
+        {/* PhotoContainer for image 51 */}
+        <PhotoContainer
+          imageUrl={image51}
+          webpUrl={image51w}
+          altText="belgrade51"
+          overlayText="A post office near the central bus station"
+          onClickHandler={handleClick}
+        />
 
-  <div className="photo-container">
-  <div className="overlay" onClick={() =>window.location.href='/belgradeairport'}>
-    <li className="nav_item">
-      <NavLink to="/belgradeairport"></NavLink>
-    </li>
-    <div className="text">A control tower in Belgrade airport and MORE</div>
-  </div>
-  <img src={image8} alt="belgrade8" className="photo" />
-</div>
+        {/* PhotoContainer for image 6 */}
+        <PhotoContainer
+          imageUrl={image6}
+          webpUrl={image6w}
+          altText="belgrade6"
+          overlayText="A building somewhere in Belgrade"
+          onClickHandler={handleClick}
+        />
 
+        {/* PhotoContainer for image 7 */}
+        <PhotoContainer
+          imageUrl={image7}
+          webpUrl={image7w}
+          altText="belgrade7"
+          overlayText="Palace Serbia, Novigrad"
+          onClickHandler={handleClick}
+        />
+
+        {/* PhotoContainer for image 8 */}
+        <PhotoContainer
+          imageUrl={image8}
+          webpUrl={image8w}
+          altText="belgrade8"
+          overlayText="A control tower in Belgrade airport and MORE"
+          onClickHandler={handleClick}
+        />
+         {/* PhotoContainer for image 9 */}
+  <PhotoContainer
+    imageUrl={image9}
+    webpUrl={image9w}
+    altText="belgrade9"
+    overlayText="A building somewhere in Belgrade"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 10 */}
+  <PhotoContainer
+    imageUrl={image10}
+    webpUrl={image10w}
+    altText="belgrade10"
+    overlayText="A building somewhere in Novi Belgrade"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 11 */}
+  <PhotoContainer
+    imageUrl={image11}
+    webpUrl={image11w}
+    altText="belgrade11"
+    overlayText="A Hotel near the central bus station"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 12 */}
+  <PhotoContainer
+    imageUrl={image12}
+    webpUrl={image12w}
+    altText="belgrade12"
+    overlayText="Музеј савремене уметности, New Belgrade"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 13 */}
+  <PhotoContainer
+    imageUrl={image13}
+    webpUrl={image13w}
+    altText="belgrade13"
+    overlayText="A building Novi Beograd"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 14 */}
+  <PhotoContainer
+    imageUrl={image14}
+    webpUrl={image14w}
+    altText="belgrade14"
+    overlayText="A building Novi Beograd"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 15 */}
+  <PhotoContainer
+    imageUrl={image15}
+    webpUrl={image15w}
+    altText="belgrade15"
+    overlayText="A block in Novi Beograd"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 16 */}
+  <PhotoContainer
+    imageUrl={image16}
+    webpUrl={image16w}
+    altText="belgrade16"
+    overlayText="A block in Novi Beograd"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 17 */}
+  <PhotoContainer
+    imageUrl={image17}
+    webpUrl={image17w}
+    altText="belgrade17"
+    overlayText="A restaurant in Novi Beograd"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 18 */}
+  <PhotoContainer
+    imageUrl={image18}
+    webpUrl={image18w}
+    altText="belgrade18"
+    overlayText="A control tower in Belgrade airport"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 19 */}
+  <PhotoContainer
+    imageUrl={image19}
+    webpUrl={image19w}
+    altText="belgrade19"
+    overlayText="A control tower in Belgrade airport"
+    onClickHandler={handleClick}
+  />
+
+  {/* PhotoContainer for image 20 */}
+  <PhotoContainer
+    imageUrl={image20}
+    webpUrl={image20w}
+    altText="belgrade20"
+    overlayText="Blocks, Novi Beograd, Serbia"
+    onClickHandler={handleClick}
+  />
+
+   {/* PhotoContainer for image 21 */}
+   <PhotoContainer
+          imageUrl={image21}
+          webpUrl={image21w}
+          altText="belgrade21"
+          overlayText="A building in Makedonska Street, Belgrade, Serbia"
+          onClickHandler={handleClick}
+        />
+
+        {/* PhotoContainer for image 22 */}
+        <PhotoContainer
+          imageUrl={image22}
+          webpUrl={image22w}
+          altText="belgrade22"
+          overlayText="An Object in front of the restaurant Dining Sailor, Belgrade"
+          onClickHandler={handleClick}
+        />
+    
 <div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image5)} >
-      <div class="text" >A building somehwhere in Belgrade</div>
-    </div>
-  <img src={image5} alt="belgrade5" className="photo" onClick={() => handleClick(image5)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image51)} >
-      <div class="text" >A post office near the central bus station</div>
-    </div>
-  <img src={image51} alt="belgrade51" className="photo" onClick={() => handleClick(image51)} />
-</div>
-
-<div className="photo-container">
-  <div class="overlay" onClick={() => handleClick(image6)} >
-      <div class="text" >A building somehwhere in Belgrade</div>
-    </div>
-  <img src={image6} alt="belgrade6" className="photo" onClick={() => handleClick(image6)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image7)} >
-      <div class="text" >Palace Serbia, Novigrad</div>
-    </div>
-  <img src={image7} alt="belgrade7" className="photo" onClick={() => handleClick(image7)} />
-</div>
-
-
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image9)} >
-      <div class="text" >A building somehwhere in Belgrade</div>
-    </div>
-  <img src={image9} alt="belgrade9" className="photo" onClick={() => handleClick(image9)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image10)} >
-      <div class="text" >A building somehwhere in Novi Belgrade</div>
-    </div>
-  <img src={image10} alt="belgrade10" className="photo" onClick={() => handleClick(image10)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image11)} >
-      <div class="text" >A Hotel near the central bus station</div>
-    </div>
-  <img src={image11} alt="belgrade11" className="photo" onClick={() => handleClick(image11)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image12)} >
-      <div class="text" >Музеј савремене уметности, new belgrade,</div>
-    </div>
-  <img src={image12} alt="belgrade12" className="photo" onClick={() => handleClick(image12)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image13)} >
-      <div class="text" >A building Novi Beograd</div>
-    </div>
-  <img src={image13} alt="belgrade13" className="photo" onClick={() => handleClick(image13)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image14)} >
-      <div class="text" >A building Novi Beograd</div>
-    </div>
-  <img src={image14} alt="belgrade14" className="photo" onClick={() => handleClick(image14)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image15)} >
-      <div class="text" >A block in Novi Beograd</div>
-    </div>
-  <img src={image15} alt="belgrade15" className="photo" onClick={() => handleClick(image15)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image16)} >
-      <div class="text" >A block in Novi Beograd</div>
-    </div>
-  <img src={image16} alt="belgrade16" className="photo" onClick={() => handleClick(image16)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image17)} >
-      <div class="text"> A restaurant in Novi Beograd</div>
-    </div>
-  <img src={image17} alt="belgrade17" className="photo" onClick={() => handleClick(image17)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image18)} >
-      <div class="text" >A control tower in Belgrade airport</div>
-    </div>
-  <img src={image18} alt="belgrade18" className="photo" onClick={() => handleClick(image18)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image19)} >
-      <div class="text" >A control tower in Belgrade airport</div>
-    </div>
-  <img src={image19} alt="belgrade19" className="photo" onClick={() => handleClick(image19)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image20)} >
-      <div class="text" >Blocks, Novi Beograd, Serbia</div>
-    </div>
-  <img src={image20} alt="belgrade20" className="photo" onClick={() => handleClick(image20)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image21)} >
-      <div class="text" >A building in Makedonska Street, Belgrade,Serbia</div>
-    </div>
-  <img src={image21} alt="belgrade21" className="photo" onClick={() => handleClick(image21)} />
-</div>
-
-<div className="photo-container">
-    <div class="overlay" onClick={() => handleClick(image22)} >
-      <div class="text" >An Object in front of the restaurant Dining Sailor, Belgrade</div>
-    </div>
-  <img src={image22} alt="belgrade22" className="photo" onClick={() => handleClick(image22)} />
-</div>
-<div className="photo-container">
 </div>
 <div className="photo-container">
     </div>
-
 
 {enlargedImg && (
           <div className="dialogue-overlay">
@@ -276,30 +382,6 @@ function UIpage() {
             </div>
           </div>
         )}
-  {/* <img src={image1} alt="belgrade1" className="photo" />
-    <img src={image2} alt="belgrade2" className="photo" />
-    <img src={image3} alt="belgrade3" className="photo" />
-    <img src={image4} alt="belgrade4" className="photo" />
-    <img src={image5} alt="belgrade5" className="photo" />
-    <img src={image6} alt="belgrade6" className="photo" />
-    <img src={image7} alt="belgrade7" className="photo" />
-    <img src={image8} alt="belgrade8" className="photo" />
-    <img src={image9} alt="belgrade9" className="photo" />
-    <img src={image10} alt="belgrade10" className="photo" />
-    <img src={image11} alt="belgrade11" className="photo" />
-    <img src={image12} alt="belgrade12" className="photo" />
-    <img src={image13} alt="belgrade13" className="photo" />
-    <img src={image14} alt="belgrade14" className="photo" />
-    <img src={image15} alt="belgrade15" className="photo" />
-    <img src={image16} alt="belgrade16" className="photo" />
-    <img src={image17} alt="belgrade17" className="photo" />
-    <img src={image18} alt="belgrade18" className="photo" />
-    <img src={image19} alt="belgrade19" className="photo" />
-    <img src={image20} alt="belgrade20" className="photo" />
-    <img src={image21} alt="belgrade21" className="photo" />
-    <img src={image22} alt="belgrade22" className="photo" />  */}
-
-      {/* Add more <img> elements for additional photos */}
     </div>
 </>
     
