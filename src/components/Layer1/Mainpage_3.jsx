d3531fdae9d4855cc4871bf9195abdd128d1983b
Yulevski <yonaiyuu@gmail.com>
2023-12-31 22:07:40 +0900

    simplifychanges

---
+++ src/components/Layer1/Mainpage_3.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {  Link , NavLink} from 'react-router-dom';
+import './styles/mainpage3.css';
+import img from './myimage2.jpg'
+
+
+function Mainpage() {
+  return (
+    <>
+      <header className="site-header3">
+        <div className="wrapper-site-header_wrapper3">
+        {/* <img src="myimage_1.jpg" alt="Logo" className="logo3" /> */}
+        <img src= {img} alt="Logo" className="logo3" />
+          {/* <h1 className="title-app3">Zen Artist</h1> */}
+          <ul className="nav_wrapper3">
+            <li className="nav_item3"><NavLink to="/">Home</NavLink></li>
+            <li className="nav_item3"><a href="https://www.instagram.com/yu4nai/" target="_blank" rel="noreferrer">Instagram</a></li>
+            {/* <li className="nav_item3"><a href="https://www.linkedin.com/in/zen-artist/" target="_blank" rel="noreferrer">Linkedin</a></li> */}
+            <li className="nav_item3"><a href="https://www.behance.net/ynilevski" target="_blank" rel="noreferrer">Behance</a></li>
+          </ul>
+        </div>
+      </header>
+
+      <div className="mainpage-container3">
+        <div className='photo-page-main3'>
+          <Link className='link3' to="https://yonaiyuu.wixsite.com/yu-s-private-photos">
+            <button className='button-main3'>Explore</button>
+            <h4 className='button-title-main3'>Private Photography</h4>
+          </Link>
+        </div>
+
+        <div className='blog-page-main3'>
+          <Link className='link3' to="https://samurailevski.blogspot.com/">
+            <button className='button-main4'>Explore</button>
+            <h4 className='button-title-main3'>Private Stories</h4>
+          </Link>
+        </div>
+      </div>{/* "mainpage-container" */}
+    </>
+  );
+}
+
+export default Mainpage;
